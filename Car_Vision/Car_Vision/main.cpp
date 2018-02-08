e20eac0e1d651597e7665d9d1e8eb91be1c31509
Unknown <pottsbrendan@rocketmail.com>
2018-02-08 23:27:23 +0000

    testing CNN with car detector
    added in secound file recog_care that will be used to test CNN against pics of cars (urresntly loading pic of cars but not crawing boxes, might be dew to running on GPU without cuda)

--- Car_Vision/Car_Vision/main.cpp
+++ Car_Vision/Car_Vision/main.cpp
@@ -1,3 +1,5 @@
+/*
+
 #include <dlib\opencv.h>
 #include <opencv2\highgui\highgui.hpp>
 #include <dlib\image_processing\frontal_face_detector.h>
@@ -13,44 +15,48 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-	try {
-		VideoCapture cap(0);
-		if (!cap.isOpened()) {
-			return -1;
-		}
-
-		image_window win;
-		frontal_face_detector detector = get_frontal_face_detector();
-		shape_predictor pose_model;
-		deserialize("S:\\dlib-19.9\\models\\shape_predictor_68_face_landmarks.dat") >> pose_model;
-
-		while (!win.is_closed()) {
-			Mat temp;
-			if (!cap.read(temp)) {
-				break;
-			}
-
-			cv_image<bgr_pixel> cimg(temp);
-			std::vector<rectangle> face = detector(cimg);
-			std::vector<full_object_detection> shapes;
-			for (unsigned long i = 0; i < face.size(); ++i) {
-				shapes.push_back(pose_model(cimg, face[i]));
-			}
-
-			win.clear_overlay();
-			win.set_image(cimg);
-			win.add_overlay(render_face_detections(shapes));
-		}
-	}
-	catch (serialization_error& e) {
-		cout << "You need dlib's default face landmarking model file to run this example." << endl;
-		cout << "You can get it from the following URL: " << endl;
-		cout << "   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
-		cout << endl << e.what() << endl;
-	}
-	catch (exception& e) {
-		cout << e.what() << endl;
-	}
-	
+try {
+VideoCapture cap(0);
+if (!cap.isOpened()) {
+return -1;
 }
 
+image_window win;
+frontal_face_detector detector = get_frontal_face_detector();
+shape_predictor pose_model;
+deserialize("S:\\dlib-19.9\\models\\mmod_front_and_rear_end_vehicle_detector.dat") >> pose_model;
+
+while (!win.is_closed()) {
+Mat temp;
+if (!cap.read(temp)) {
+break;
+}
+
+cv_image<bgr_pixel> cimg(temp);
+std::vector<rectangle> face = detector(cimg);
+std::vector<full_object_detection> shapes;
+for (unsigned long i = 0; i < face.size(); ++i) {
+shapes.push_back(pose_model(cimg, face[i]));
+}
+
+win.clear_overlay();
+win.set_image(cimg);
+win.add_overlay(render_face_detections(shapes));
+}
+}
+catch (serialization_error& e) {
+cout << "You need dlib's default face landmarking model file to run this example." << endl;
+cout << "You can get it from the following URL: " << endl;
+cout << "   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
+cout << endl << e.what() << endl;
+}
+catch (exception& e) {
+cout << e.what() << endl;
+}
+
+}
+
+
+
+
+*/
